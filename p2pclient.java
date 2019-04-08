8d42f86cf3870e85f0a9a011a68bec161bbf0675
brandon-holmes <43683359+brandon-holmes@users.noreply.github.com>
2019-04-08 17:19:19 -0400

    Add files via upload

--- p2pclient.java
+++ p2pclient.java
@@ -12,10 +12,14 @@ public class p2pclient {
 	private static int port = 20380;
 	private byte[] buff;
 	
+	
 	public static void main(String [] args) {
 		//run init, getting ip addresses of DHT servers
 		boolean running = true;
-		
+		try {
+			address = InetAddress.getByName("10.10.10.10");
+		} catch (UnknownHostException e1) {
+		}
 		
 		while(running) {
 			System.out.println("input 'S' to store content in DHT server \n"
@@ -27,24 +31,14 @@ public class p2pclient {
 			String choice = usrInput.next();
 			if( choice.equalsIgnoreCase("S")) { // Store
 				
-			}
-			else if (choice.equalsIgnoreCase("Q")) { // searches 						
-				System.out.println("Please type the exact name of the file you are looking for: ");
+				System.out.println("Please type the exact name of the file you wish to upload: ");
 				String file = usrInput.next();
 				
-				try {
-					address = InetAddress.getByName("10.10.10.10");
-				} catch (UnknownHostException e) {
-					System.out.println("Query Failure: Ip incorrect. \n");
-					continue;
-				}
-				
-				
 				int fileHash = computeHash(file);
-				int realPort = port + (fileHash%4);
+				int realPort = port + ((fileHash%4)*2-2);
 				byte[] request = file.getBytes();
 				byte[] requestType = "1".getBytes();
-				DatagramPacket packet = new DatagramPacket(requestType, requestType.length, address, port);
+				DatagramPacket packet = new DatagramPacket(requestType, requestType.length, address, realPort);
 				String responseStr;
 				try {
 					socket.send(packet);
@@ -57,22 +51,69 @@ public class p2pclient {
 					socket.receive(response);
 					responseStr = new String(packet.getData(),0, packet.getLength());
 				} catch (IOException e) {
-					System.out.println("Query Failure: No response from server ");
+					System.out.println("Query Failure: No response from server \n");
 					continue;
 				}
 				if(responseStr.equals("response")){
-					DatagramPacket query = new DatagramPacket(request, request.length, address, port);
+					DatagramPacket query = new DatagramPacket(request, request.length, address, realPort);
 					try {
 						socket.send(query);
 					} catch (IOException e) {
-						System.out.println("Query Failure: Unable to send query ");
+						System.out.println("Query Failure: Unable to send query \n");
 						continue;
 					}
 					DatagramPacket queryInfo = new DatagramPacket(request, request.length);
 					try {
 						socket.receive(queryInfo);
 					} catch (IOException e) {
-						System.out.println("Query Failure: Did not receive query information. ");
+						System.out.println("Query Failure: Did not receive query information. \n");
+						continue;
+					}
+					String queryInfoStr = new String(queryInfo.getData(),0, queryInfo.getLength());
+					System.out.println(queryInfoStr);
+				}
+				else {
+					System.out.println("Server sent incorrect response");
+				}
+			
+			}
+			else if (choice.equalsIgnoreCase("Q")) { // searches 						
+				System.out.println("Please type the exact name of the file you are looking for: \n");
+				String file = usrInput.next();				
+				
+				int fileHash = computeHash(file);
+				int realPort = port + ((fileHash%4)*2-2);
+				byte[] request = file.getBytes();
+				byte[] requestType = "2".getBytes();
+				DatagramPacket packet = new DatagramPacket(requestType, requestType.length, address,  realPort);
+				String responseStr;
+				try {
+					socket.send(packet);
+				} catch (IOException e) {
+					System.out.println("Query Failure: failed to send packet.\n");
+					continue;
+				}
+				DatagramPacket response = new DatagramPacket(requestType, requestType.length);
+				try {
+					socket.receive(response);
+					responseStr = new String(packet.getData(),0, packet.getLength());
+				} catch (IOException e) {
+					System.out.println("Query Failure: No response from server \n");
+					continue;
+				}
+				if(responseStr.equals("response")){
+					DatagramPacket query = new DatagramPacket(request, request.length, address,  realPort);
+					try {
+						socket.send(query);
+					} catch (IOException e) {
+						System.out.println("Query Failure: Unable to send query \n");
+						continue;
+					}
+					DatagramPacket queryInfo = new DatagramPacket(request, request.length);
+					try {
+						socket.receive(queryInfo);
+					} catch (IOException e) {
+						System.out.println("Query Failure: Did not receive query information. \n");
 						continue;
 					}
 					String queryInfoStr = new String(queryInfo.getData(),0, queryInfo.getLength());
